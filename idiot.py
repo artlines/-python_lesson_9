e90ad906dfbb5c826880a1347a281c7f16ff3498
Artur <luart004@gmail.com>
2020-03-31 20:04:24 +0500

    Рализация простейшей логики игры: пользователь ходит, компьютер отбивается.

--- idiot.py
+++ idiot.py
@@ -1,95 +1,165 @@
 import random
-import sys
+import json
+import requests
 
 
 class Idiot:
-    cards_deck, user_range, comp_range = {}, {'cards': {}, 'active': True}, {'cards': {}, 'active': False}  # колода, карты пользователя, карты компьютера
-    actions = {'n': 'бито, следующий ход', 't': 'не могу отбить, взял'} # доступные опции; @todo перевести, подкинуть
+    cards_deck, user_range, comp_range = {}, {'cards': {}, 'active': True}, {'cards': {},
+                                                                             'active': False}  # колода, карты пользователя, карты компьютера
+    actions = {'n': 'бито, следующий ход', 't': 'не могу отбить, взял'}  # доступные опции; @todo перевести, подкинуть
     card_types = ['Черви', 'Бубны', 'Трефы', 'Пики']
     cards_quality = {'Шестёрка': 0, 'Семёрка': 1, 'Восьмёрка': 2, 'Девятка': 3, 'Десятка': 4, 'Валет': 5, 'Дама': 6,
                      'Король': 7, 'Туз': 8}
     cards_count_set = 6  # по сколько карт сдаём
     set_cards = {}  # все карты кона
-    set_count = 1 # какой по счёту кон
+    set_count = 0  # какой по счёту кон
 
     def __init__(self):
         for type in self.card_types:
             self.cards_deck[type] = list(self.cards_quality.keys())
 
-        print(f'\nТусуем колоду, раскидываем картишки: козырь {random.choice(self.card_types)}\n')
-
-        self._get_cards(self.user_range)
-        self._get_cards(self.comp_range)
+        print(f'\n__Тусуем колоду, раскидываем картишки: козырь {random.choice(self.card_types)}__\n')
+        self._new_set_init()
 
         # @todo определить, кто ходит - найти в картах самый маленький козырь
-        try:
-            self.start()
-        except Exception as e:
-            print("Ошибка! ", e)
+        self.user_step()
 
-    def start(self):
-        print('Я: ', self.user_range)
-        print('Компьютер: ', self.comp_range)
-        print('Колода: ', self.cards_deck)
-        print('\n__Кон № {}__\n'.format(self.set_count))
+    # ход игрока
+    def user_step(self):
+        self.set_cards.clear()
 
+        if len(self.user_range['cards'].keys()) == 0:
+            print("___!!!__Вы выиграли!__!!!___")
+            exit(8)
+
+        print("__Доступные карты__")
+        for key, card in self.user_range['cards'].items():
+            print(key, '-', ' '.join(card['name']))
+
+        print("__Доступные действия__")
         for x, y in self.actions.items():
             print(f"{x} - {y}")
-        step = input('\nВыберите карту по номеру или действие ')
 
+        step = input('\nВаш ход: выберите карту по номеру или действие ')
+
+        # обработка ввода
         if step == 'n':
-            self.set_cards = {}
+            if len(self.set_cards.keys()) > 0:
+                self.set_cards.clear()
+                print('__Бито, следующий ход__')
+                return self.user_step()
+            else:
+                raise Exception('Нет карт для сброса')
         elif step == 't':
-            self.user_range['cards'] = {**self.user_range['cards'], **self.set_cards}
-            print('__Взял__', self.user_range['cards'])
+            if len(self.set_cards.keys()) > 0:
+                self.user_range['cards'] = {**self.user_range['cards'], **self.set_cards}
+                print('__Взял__', self.user_range['cards'])
+                return self.comp_step()
+            else:
+                raise Exception('Нет карт к принятию')
         elif step.isdigit():
             key = int(step)
             if key in list(self.user_range['cards'].keys()):
                 # извлечь из моей колоды карту
-                self.set_cards[f"user_{self.set_count}"] = self.user_range['cards'][key]
-                self.comp_range['active'], self.user_range['active'] = True, False
+                self.set_cards[f"user_{self.set_count}"] = self.user_range['cards'].pop(key, None)
                 print('__Вы сходили картой__', self.set_cards)
+                return self.comp_step()
             else:
                 raise Exception('У Вас нет такой карты!')
         else:
             raise Exception('Не нашлось доступного действия: пожалуйста, повторите')
 
-        return self.comp_step()
-
+    # ход компьютера
     def comp_step(self):
-        print('\n__Ходит компьютер__')
-        print('__Карты кона__', self.set_cards)
-        # здесь будет ход - отбиться или взять
+        if len(self.comp_range['cards'].keys()) == 0:
+            print("___!!!__Компьтер выиграл!__!!!___")
+            exit(8)
 
-        self.user_range['active'], self.comp_range['active'] = True, False
-        if self._new_set_init():
-            return self.start()
+        print('\n__Ходит компьютер__')
+        user_card = self.set_cards[f'user_{self.set_count}']
+        comp_cards = self.comp_range['cards']
+        user_card_type = user_card['name'][1]
+        user_card_quality = user_card['quality']
+
+        try:
+            comp_card_answer = next(
+                x for x in comp_cards.values() if x['quality'] > user_card_quality and x['name'][1] == user_card_type
+            )
+            print('__Компьютер отбивается картой__', comp_card_answer)
+            self.set_cards[f"comp_{self.set_count}"] = comp_card_answer
+            print('__Карты кона__', self.set_cards)
+            self._check_step()
+            self.user_range['active'], self.comp_range['active'] = True, False
+            if self._new_set_init():
+                return self.user_step()
+        except StopIteration:
+            print('__Нечем биться, компьютер берёт__')
+            comp_cards_count = len(self.comp_range['cards'].keys())
+            self.comp_range['cards'].update({comp_cards_count: user_card})
+            if self._new_set_init():
+                return self.user_step()
+        except Exception as e:
+            raise e
 
     # сдать карты в начале, добрать после кона
     def _get_cards(self, cards_range):
-        self._check_cards_deck()
         i = 0
         # если карты ещё есть, сдавать
         while len(cards_range['cards'].items()) < self.cards_count_set:
-            new_type = random.choice(self.card_types) # self._check_cards_deck()
-            random.shuffle(self.cards_deck[new_type])
-            new_quality = self.cards_deck[new_type].pop()
-            cards_range['cards'][i] = {'name': (new_type, new_quality), 'quality': self.cards_quality[new_quality]}
-            i += 1
+            new_type = self._check_cards_deck()
+            if not new_type:
+                print('__Колода закончилась: играем с оставшимися картами__')
+                return
+            else:
+                random.shuffle(self.cards_deck[new_type])
+                new_quality = self.cards_deck[new_type].pop()
+                cards_range['cards'][i] = {'name': (new_quality, new_type), 'quality': self.cards_quality[new_quality]}
+                i += 1
 
         return cards_range
 
+    # действия в начале нового кона
     def _new_set_init(self):
         self.set_count += 1
         self._get_cards(self.user_range)
         self._get_cards(self.comp_range)
 
+        print('Мои карты: ', self.user_range)
+        print('Карты компьютера: ', self.comp_range)
+        print('Колода: ', self.cards_deck)
+        print('\n__Кон № {}__\n'.format(self.set_count))
+
         return True
 
+    # проверить, не закончились ли карты в масти, выбрать новую масть, если да
     def _check_cards_deck(self):
-        # проверить, не закончились ли карты в масти, выбрать новую масть, если да
-        # если все масти закончились, раздача карт завершается, игра идёт до последней карты
-        pass
+        new_type = random.choice(self.card_types) if len(self.card_types) > 0 else False
+        if new_type and len(self.cards_deck[new_type]) == 0:
+            print('__Карты масти {}  закончились__'.format(new_type))
+            self.cards_deck.pop(new_type)
+            self.card_types.remove(new_type)
+            return self._check_cards_deck()
+
+        return new_type
+
+    # как играть
+    def _check_step(self):
+        active_key = 'user' if self.user_range['active'] else 'comp'
+        active_card = self.set_cards.pop(f'{active_key}_{self.set_count}')
+        cards_for_compare = [active_card, list(self.set_cards.values())[0]]
+        is_quality = cards_for_compare[0]['quality'] < cards_for_compare[1]['quality']
+        is_type = cards_for_compare[0]['name'][1] == cards_for_compare[1]['name'][1]
+
+        if not is_quality:
+            raise Exception('Карта не бьётся по величине')
+        elif not is_type:
+            # @todo добавить поддержку козырей
+            raise Exception('Карта не бьётся по масти')
+        elif is_quality and is_type:
+            print('Карта бита, следующий кон')
+            return True
+
+        return False
 
 
 i = Idiot()
